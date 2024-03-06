bbf66a735404083d024b3fccdd0e3eee8da74cb8
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-03-06 16:13:24 +0530

    .

--- js/app.js
+++ js/app.js
@@ -1,32 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const mainId = document.getElementById('header')
-    const nav = getElementAndEvent('.fa-bars', 'click', handleNav, null);
-    const navclass = document.querySelector('.fa-bars');
-    function handleNav(e) {
-        if (e.target) {
-            navclass.style.visibility = 'hidden';
+    const navBar = document.querySelector('.fa-bars');
+    const close = document.querySelector('.fa-close')
+    const ul = document.querySelector('ul');
+    const logo = document.querySelector('.logo');
+    const content = document.querySelector('.content')
+    const animation = document.querySelector('.anim');
+    const hero = document.querySelector('.hero');
+    navBar.addEventListener('click', handleClick);
+    close.addEventListener('click', handleCloseClick);
+    document.addEventListener('scroll', handleScroll);
+    function handleClick(element) {
+        if (element) {
+            ul.classList.add('buffer');
+            logo.style.visibility = 'hidden';
+            close.style.visibility = 'visible';
+            navBar.style.visibility = 'hidden';
         }
-        const div1 = createEl('div', 'div1', 'heyy', mainId);
-        const divId = document.getElementById('div1');
-        div1.style.background = 'green';
-        mainId.style.visibility = 'visible';
-        mainId.appendChild(div1);
-        mainId.append()
-        // div1.style.zIndex=-1;
-        // document.body.style.zIndex
-        // console.log(e);
+    };
+    function handleCloseClick(element) {
+        if (element) {
+            ul.classList.remove('buffer');
+            logo.style.visibility = 'visible';
+            close.style.visibility = 'hidden';
+            navBar.style.visibility = 'visible';
+        }
     }
-    function createEl(element, id, txt, append) {
-        const el = document.createElement(element);
-        el.id = id;
-        el.innerText = txt;
-        mainId.append(el)
-        return el;
-    }
-    function getElementAndEvent(clas, event, eventFun, text) {
-        const navId = document.querySelector(clas);
-        navId.addEventListener(event, eventFun);
-        navId.innerText = text;
-        return text;
+    function handleScroll(element) {
+        // document.classList.add('anim');
     }
 })
